5a41e884df0076d09d351ef3d72a9a8305aa1442
Mayank saini <mayanksaini4455@gmail.com>
2023-05-20 20:59:50 +0530

    Add products backend and forntend part

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
 import News from "./modal/News.js"
+import CMB from "./modal/CMB.js"
+import Cookies from "./modal/Cookies.js"
+import HC from "./modal/HC.js"
+import RBread from "./modal/RBread.js"
+import SBread from "./modal/SBread.js"
+import TPK from "./modal/TPK.js"
 
 
 
@@ -46,6 +52,163 @@ app.post("/news", async (req, res) => {
             return res.status(400).json({ msg: "Something went wrong" });
     } catch (error) {
         return res.status(400).json({ msg: error })
+    }
+})
+
+app.post("/products" , async (req,res)=>
+{
+    const {name,desc,catag} = req.body.value;
+    const {url,public_id} = req.body;
+
+    if(catag === "RBread")
+    {
+            try {
+                const response = await RBread.create({name,desc,catag,url,public_id})
+                if(response)
+                    return res.status(200).json({msg:"Product succcesFully created"})
+                else
+                    return res.status(400).json({msg:"Something went wrong"})
+            } catch (error) {
+                
+            }
+    }
+    else if(catag === "SBread" )
+    {
+        try {
+            const response = await SBread.create({name,desc,catag,url,public_id})
+            if(response)
+                return res.status(200).json({msg:"Product succcesFully created"})
+            else
+                return res.status(400).json({msg:"Something went wrong"})
+        } catch (error) {
+            
+        }
+    }
+    else if(catag === "CMB")
+    {
+        try {
+            const response = await CMB.create({name,desc,catag,url,public_id})
+            if(response)
+                return res.status(200).json({msg:"Product succcesFully created"})
+            else
+                return res.status(400).json({msg:"Something went wrong"})
+        } catch (error) {
+            
+        }
+    }
+    else if(catag === "TPK")
+    {
+        try {
+            const response = await TPK.create({name,desc,catag,url,public_id})
+            if(response)
+                return res.status(200).json({msg:"Product succcesFully created"})
+            else
+                return res.status(400).json({msg:"Something went wrong"})
+        } catch (error) {
+            
+        }
+    }
+    else if(catag === "Cookie")
+    {
+        try {
+            const response = await Cookies.create({name,desc,catag,url,public_id})
+            if(response)
+                return res.status(200).json({msg:"Product succcesFully created"})
+            else
+                return res.status(400).json({msg:"Something went wrong"})
+        } catch (error) {
+            
+        }
+    }
+    else {
+        try {
+            const response = await HC.create({name,desc,catag,url,public_id})
+            if(response)
+                return res.status(200).json({msg:"Product succcesFully created"})
+            else
+                return res.status(400).json({msg:"Something went wrong"})
+        } catch (error) {
+            
+        }
+    }
+})
+
+
+app.get("/product/RBread",async(req,res)=>
+{
+    try {
+        const response = await RBread.find();
+        if(response)
+            return res.status(200).json({msg:"ALL posts" , response});
+        else 
+            return res.status(400).json({msg:"Something went wrong"});
+    } catch (error) {
+        return res.status(400).json({msg:"Something went wrong"});
+    }
+})
+
+app.get("/product/Cookies",async(req,res)=>
+{
+    try {
+        const response = await Cookies.find({})
+        if(response)
+            return res.status(200).json({msg:"ALL posts" , response});
+        else 
+            return res.status(400).json({msg:"Something went wrong"});
+    } catch (error) {
+        return res.status(400).json({msg:"Something went wrong"});
+    }
+})
+
+app.get("/product/CMB", async(req,res)=>
+{
+    try {
+        const response = await CMB.find({})
+        if(response)
+            return res.status(200).json({msg:"ALL posts" , response});
+        else 
+            return res.status(400).json({msg:"Something went wrong"});
+    } catch (error) {
+        return res.status(400).json({msg:"Something went wrong"});
+    }
+})
+
+app.get("/product/TPK",async(req,res)=>
+{
+    try {
+        const response = await TPK.find({})
+        if(response)
+            return res.status(200).json({msg:"ALL posts" , response});
+        else 
+            return res.status(400).json({msg:"Something went wrong"});
+    } catch (error) {
+        return res.status(400).json({msg:"Something went wrong"});
+    }
+})
+
+app.get("/product/SBread",async(req,res)=>
+{
+    try {
+        const response = await SBread.find({})
+        if(response)
+            return res.status(200).json({msg:"ALL posts" , response});
+        else 
+            return res.status(400).json({msg:"Something went wrong"});
+    } catch (error) {
+        return res.status(400).json({msg:"Something went wrong"});
+    }
+})
+
+app.get("/product/HC", async (req,res)=>
+{
+    try {
+        const response = await HC.find({})
+        if(response)
+            return res.status(200).json({msg:"ALL posts" , response});
+        else 
+            return res.status(400).json({msg:"Something went wrong"});
+    } catch (error) {
+        return res.status(400).json({msg:"Something went wrong"});
     }
 })
 
---
+++ server/modal/CMB.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const ProductSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String
+        },
+        desc: {
+            type: String
+        },
+        catag: {
+            type: String
+        },
+        url: {
+            type: String
+        },
+        public_id: {
+            type: String
+        }
+
+    }, { timestamps: true }
+)
+
+
+const schema = mongoose.model("CakeMuffinBrownies", ProductSchema);
+
+export default schema;
